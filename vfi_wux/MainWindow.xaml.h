449be83a852717aebb849afc198eae2db78eefa7
Matt Gradwohl <mattgr@msn.com>
2021-12-13 17:13:18 -0800

    Rename branch to main

--- vfi_wux/MainWindow.xaml.h
+++ vfi_wux/MainWindow.xaml.h
@@ -13,7 +13,6 @@ namespace winrt::vfi_wux::implementation
         int32_t MyProperty();
         void MyProperty(int32_t value);
 
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
         void AppBarButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
     };
 }
